45448b4a2472eab0de48ba495acc5539de04be0d
SeanSin <seansin@chungbuk.ac.kr>
2020-08-24 15:39:35 +0900

    The Java, Java8 2강 완료
    자바에서 기본적으로 제공하는 다양한 함수형 인터페이스 형태와 제공하는 default 메소드들에 대해서 알게되었고, 구현 및 사용법을 습득하게됌.

--- java8/src/main/java/Foo.java
+++ java8/src/main/java/Foo.java
@@ -1,6 +1,31 @@
+import java.util.function.*;
+
 public class Foo {
     public static void main(String[] args) {
-        Plus10 plus10 = new Plus10();
-        System.out.println(plus10.apply(1));
+        Function<Integer, Integer> plus10 = (number) -> number + 10;
+        Function<Integer, Integer> multiple2 = (number) -> number * 2;
+
+        UnaryOperator<Integer> unaryPlus10 = (number) -> number + 10;
+        UnaryOperator<Integer> unaryMultiple2 = (number) -> number * 2;
+
+        Function<Integer, Integer> multiple2AndPlus10 = plus10.compose(multiple2);
+        System.out.println(multiple2AndPlus10.apply(10));
+
+        Function<Integer, Integer> plus10AndMultiple2 = plus10.andThen(multiple2);
+        System.out.println(plus10AndMultiple2.apply(10));
+
+        Consumer<Integer> printT = integer -> System.out.println(integer);
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+        get10.get();
+
+        Predicate<String> startWithSean = (s) -> s.startsWith("sean");
+        System.out.println(startWithSean.test("sin"));
+
+        Predicate<Integer> isOdd = (i) -> i%2 == 1;
+        System.out.println(isOdd.test(3));
+
+
     }
 }
